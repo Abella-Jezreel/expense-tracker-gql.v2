ac89bc98e88c3c79ecbac5f7cb164cd89af015a9
Abella <jezreel.abella@accenture.com>
2024-07-25 21:42:13 +0800

    Passport configured

--- backend/index.js
+++ backend/index.js
@@ -9,15 +9,58 @@ import mergedTypeDefs from "./typeDefs/index.js"
 import connectDB from './db/connectDB.js';
 import dotenv from 'dotenv';
 
+import passport from "passport";
+import session from "express-session";
+import connectMongo from "connect-mongodb-session";
+
+import { buildContext } from "graphql-passport";
+import { configurePassport } from "./passport/passport.config.js";
+
+ 
+// Load environment variables
+dotenv.config();
+
+// Configure Passport
+await configurePassport();
+
+
 // Required logic for integrating with Express
 const app = express();
 // Our httpServer handles incoming requests to our Express app.
 // Below, we tell Apollo Server to "drain" this httpServer,
 // enabling our servers to shut down gracefully.
 const httpServer = http.createServer(app);
+
+// Create a new MongoDB session store
+const MongoDBStore = connectMongo(session);
+const store = new MongoDBStore({
+  uri: process.env.MONGO_URI,
+  collection: "sessions",
+});
  
-// Load environment variables
-dotenv.config();
+// Catch errors
+store.on("error", function (error) {
+  console.log(error);
+});
+
+// Express session middleware
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false, // don't save session if unmodified
+    saveUninitialized: false,
+    store: store,
+    cookie: { 
+      maxAge: 1000 * 60 * 60 * 24 * 7, // 1 day
+      httpOnly: true, // don't let JS code access cookies
+    },
+  })
+);
+
+// Passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
 
 // Connect to MongoDB
 await connectDB();
@@ -35,12 +78,15 @@ await server.start();
 // and our expressMiddleware function.
 app.use(
   '/',
-  cors(),
+  cors({
+    origin: 'http://localhost:3000',
+    credentials: true,
+  }),
   express.json(),
   // expressMiddleware accepts the same arguments:
   // an Apollo Server instance and optional configuration options
   expressMiddleware(server, {
-    context: async ({ req }) => ({ token: res }),
+    context: async ({ req, res }) => buildContext({ req, res }),
   }),
 );
 
--- backend/resolvers/user.resolver.js
+++ backend/resolvers/user.resolver.js
@@ -2,8 +2,8 @@ import { users } from '../dummyData/data.js';
 
 const userResolver = {
     Query: {
-        users: () => users,
-        user: (_, { _id }) => users.find((user) => user._id === _id)
+        users: (_, {req, res}) => users,
+        user: (_, { _id }, {req, res}) => users.find((user) => user._id === _id)
     },
     Mutation: {}
 };
