ed485fd996f51405efe0c1ceb9e96f0dbe6b9f65
Abella <jezreel.abella@accenture.com>
2024-07-26 16:51:43 +0800

    Set Up front end

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 function App() {
   return (
     <>
-      <div className="flex items-center justify-center text-red-500 min-h-screen text-5xl bg-blue-500">
+      <div className="flex items-center justify-center text-red-500 min-h-screen text-5xl">
         <h1>Hello world</h1>
       </div>
     </>
@@ -9,3 +9,4 @@ function App() {
 }
 
 export default App;
+
